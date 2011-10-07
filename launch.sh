3947e3d7707bdfb256c482d2ff075dda0199e7ea
youBot development PC <r.j.w.wilterdink@utwente.nl>
2011-10-07 12:02:49 +0200

    new launch.sh -Robert

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /opt/ros_toolchain/setup.bash;
+source /opt/orocos_toolchain_ros/env.sh;
+
+export RTT_MOTION_CONTROL_MSGS_PATH=$(rospack find rtt_motion_control_msgs)/lib
+export YOUBOT_OODL_PATH=$(rospack find YouBot_OODL)/lib/orocos
+export RTT_PATH=$(rospack find rtt)/install
+export OCL_PATH=$(rospack find ocl)/lib/orocos
+
+export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}
+
+roslaunch $1 $2 $3 $4 $5
