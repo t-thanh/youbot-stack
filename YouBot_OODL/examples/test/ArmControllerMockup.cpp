c53cc7a3e2417788efe4c4f726b3b6ae2600cc6d
Robert Wilterdink <r.j.w.wilterdink@utwente.nl>
2011-10-28 15:44:18 +0200

    Changed OODL->youbot for easier interoperability between scripts.

--- YouBot_OODL/examples/test/ArmControllerMockup.cpp
+++ YouBot_OODL/examples/test/ArmControllerMockup.cpp
@@ -141,10 +141,10 @@ namespace YouBot
 			return false;
 		}
 
-		TaskContext* task_ptr = getPeer("OODL");
+		TaskContext* task_ptr = getPeer("youbot");
 		if(task_ptr == NULL)
 		{
-			log(Error) << "Could not find peer OODL" << endlog();
+			log(Error) << "Could not find peer YouBot_OODL" << endlog();
 			return false;
 		}
 		op_setControlModes = task_ptr->provides("Arm1")->getOperation("setControlModes");
