d69a3c6d515966968619d6fbad79097505dcd656
Robert Wilterdink <r.j.w.wilterdink@utwente.nl>
2011-10-29 19:54:28 +0200

    Fixed joystick example.

--- YouBot_OODL/include/YouBotTypes.hpp
+++ YouBot_OODL/include/YouBotTypes.hpp
@@ -6,6 +6,9 @@
 
 namespace YouBot
 {
+	enum arm_settings {NR_OF_ARM_SLAVES=5};
+	enum base_settings {NR_OF_BASE_SLAVES=4};
+
 	enum ctrl_modes {PLANE_ANGLE = 1, ANGULAR_VELOCITY = 2 , TORQUE = 3, MOTOR_STOP = 4, TWIST = 5};
 
 	typedef YouBot_OODL::motor_statuses::_flags_type::value_type motor_status;
