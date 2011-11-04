be202be10115e4aef9ed821708a238bcb61304dd
youBot development PC <r.j.w.wilterdink@utwente.nl>
2011-11-04 15:24:43 +0100

    more electric -Robert

--- YouBot_deployment/rtt_deploy/debug.sh
+++ YouBot_deployment/rtt_deploy/debug.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-export LUA_PATH=";;;/home/youbot/DEV/KUL/rFSM/?.lua"
-source /opt/ros_toolchain/setup.bash;
-source /opt/orocos_toolchain_ros/env.sh;
+
+source /opt/electric/ros/setup.bash;
+source /opt/electric/orocos_toolchain_ros/orocos_toolchain/env.sh;
+
+export YOUBOT_PATH=$(rosstack find YouBot)
+
+RTTLUA_MODULES=`rospack find ocl`/lua/modules/?.lua
+if [ "x$LUA_PATH" == "x" ]; then
+    LUA_PATH=";;"
+fi
+export LUA_PATH="$LUA_PATH;$RTTLUA_MODULES;$YOUBOT_PATH/rFSM/?.lua"
+
+echo $LUA_PATH
 
 export RTT_MOTION_CONTROL_MSGS_PATH=$(rospack find rtt_motion_control_msgs)/lib
 export YOUBOT_ADAPTERS=$(rospack find YouBot_adapters)/lib/orocos
 export YOUBOT_OODL_PATH=$(rospack find YouBot_OODL)/lib/orocos
-export RTT_PATH=$(rospack find rtt)/install
-export OCL_PATH=$(rospack find ocl)
+export RTT_PATH=$(rospack find rtt)/../install
+export OCL_PATH=$(rospack find ocl)/../install
 
 export RTT_COMPONENT_PATH=${RTT_PATH}:${OCL_PATH}/lib/orocos/gnulinux:${RTT_MOTION_CONTROL_MSGS_PATH}:${YOUBOT_OODL_PATH}:${YOUBOT_ADAPTERS}
 
